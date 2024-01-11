5134f6687b1cb4d921e618e319c8613e4d9d6b9d
chrispwd <chrispwd@protonmail.com>
2024-01-11 14:46:02 -0700

    chore/update font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "UbuntuMono Nerd Font Mono:style=Regular:pixelsize=16";
+static char *font = "Hack Nerd Font Mono:style=Medium:pixelsize=14";
 static int borderpx = 2;
 
 /*
